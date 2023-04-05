7ccd5a64e7147dfe50e9969666d696b5f52a0f33
Valentin <nakama3942@gmail.com>
2023-04-05 13:08:19 +0300

    Implemented the TextBuffer class in test.py for the next update

--- qt_colored_logger/test.py
+++ qt_colored_logger/test.py
@@ -1,59 +1,92 @@
-import sys, time
+import sys
 
-class Logger:
-    def __init__(self):
-        self.text_buffer = []
+class TextBuffer:
+	def __init__(self):
+		self._text_buffer: list[str] = []
+		self._cursor_string: int = 0
+		self._buffer_size: int = 0
 
-    def log(self, message):
-        self.text_buffer.append(message)
+	def append(self, message: str):
+		self._text_buffer.append(message)
+		self._buffer_size = len(self._text_buffer)
 
-class ConsoleLogger:
-	def __init__(self, logger):
-		super().__init__()
-		self.logger = logger
+	def replace(self, message: str, number_string: int):
+		try:
+			self._text_buffer[number_string] = message
+		except IndexError:
+			while len(self._text_buffer) != number_string + 1:
+				self.append("")
+			self.append(message)
 
 	def update_console(self):
-		if len(self.logger.text_buffer) == 2:
-			sys.stdout.write(f'\r\033[J')
-		else:
-			sys.stdout.write(f'\033[{len(self.logger.text_buffer) - 2}A\r\033[J')
-		sys.stdout.write('\n'.join(self.logger.text_buffer))  # Заменяем содержимое строки в консоли
-		sys.stdout.flush()  # Очищаем буфер вывода, чтобы изменения сразу отобразились
+		match self._buffer_size:
+			case 1:
+				pass
+			case 2:
+				sys.stdout.write(f'\r\033[K')
+			case _:
+				sys.stdout.write(f'\033[{self._cursor_string}A\r\033[J')
+		sys.stdout.write('\n'.join(self._text_buffer))
+		sys.stdout.flush()  # Clearing the output buffer so that the changes are displayed immediately
+		self._cursor_string = self._buffer_size - 1
 
 if __name__ == "__main__":
-	logger = Logger()
-	console_logger = ConsoleLogger(logger)
+	from time import sleep
+	buffer = TextBuffer()
 
-	logger.log("111")
-	console_logger.update_console()
+	buffer.append("111")
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.append("222")
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("222")
-	console_logger.update_console()
+	buffer.append("333")
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.append("444")
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("333")
-	console_logger.update_console()
+	buffer.append("555")
+	buffer.replace("15", 2)
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.replace("9", 3)
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("444")
-	console_logger.update_console()
+	buffer.append("666")
+	buffer.replace("8888", 1)
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.append("777")
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("555")
-	console_logger.update_console()
-	logger.text_buffer[2] = "15"
+	buffer.append("888")
+	buffer.replace("55", 5)
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.append("999")
+	buffer.replace("6969", 0)
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("666")
-	console_logger.update_console()
+	buffer.replace("88888888", 1)
+	buffer.update_console()
+	sleep(1)
 
-	time.sleep(1)
+	buffer.append("111111")
+	buffer.update_console()
+	sleep(1)
 
-	logger.log("777")
-	console_logger.update_console()
+	buffer.append("121212")
+	buffer.replace("10", 8)
+	buffer.update_console()
+	sleep(2)
